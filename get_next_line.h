a472911fc3afd2f07b9bc5fa87a5d34775d238c1
Florian MADURA <fmadura@e3r5p21.42.fr>
2018-05-02 12:57:40 +0200

    new

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmadura <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/23 19:59:06 by fmadura           #+#    #+#             */
+/*   Updated: 2018/01/08 14:09:54 by fmadura          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 100
+# include "libft.h"
+# include <limits.h>
+
+int				get_next_line(const int fd, char **line);
+#endif
